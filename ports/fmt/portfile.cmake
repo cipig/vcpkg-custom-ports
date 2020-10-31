fa99c5501fa3caab027d6f89b560870caca83f7c
romanszterg <rmscastle@gmail.com>
2020-10-31 07:58:51 +0100

    update fmt

--- ports/fmt/portfile.cmake
+++ ports/fmt/portfile.cmake
@@ -1,15 +1,15 @@
 vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO Komodoplatform/fmt
-        REF 4fe0b11195b7cd71f39253c44db2c9dddf6b82d4#version 7.1.0
-        SHA512 1cb2eac5abcec0a002c505a3de04284f89a6b31923edb6868cf9e9ac2803c3985ab62e2bff55da49a30def8476bfed205cf6a5b40a03a1d3df1292feb5352e86
-        HEAD_REF master
-        PATCHES fix-warning4189.patch
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KomodoPlatform/fmt
+    REF 4fe0b11195b7cd71f39253c44db2c9dddf6b82d4#version 7.1.0
+    SHA512 1cb2eac5abcec0a002c505a3de04284f89a6b31923edb6868cf9e9ac2803c3985ab62e2bff55da49a30def8476bfed205cf6a5b40a03a1d3df1292feb5352e86
+    HEAD_REF master
+    PATCHES fix-warning4189.patch
 )
 vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        PREFER_NINJA
-        OPTIONS
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
         -DFMT_CMAKE_DIR=share/fmt
         -DFMT_TEST=OFF
         -DFMT_DOC=OFF
@@ -34,27 +34,27 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
     endif()
 
     vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/fmt/core.h
-            "defined(FMT_SHARED)"
-            "1"
-            )
+        "defined(FMT_SHARED)"
+        "1"
+    )
 endif()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 vcpkg_fixup_cmake_targets()
-#vcpkg_fixup_pkgconfig()
+vcpkg_fixup_pkgconfig()
 
 if(VCPKG_TARGET_IS_WINDOWS)
     if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
         vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/share/fmt/fmt-targets-debug.cmake
-                "lib/fmtd.dll"
-                "bin/fmtd.dll"
-                )
+            "lib/fmtd.dll"
+            "bin/fmtd.dll"
+        )
     endif()
     if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
         vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/share/fmt/fmt-targets-release.cmake
-                "lib/fmt.dll"
-                "bin/fmt.dll"
-                )
+            "lib/fmt.dll"
+            "bin/fmt.dll"
+        )
     endif()
 endif()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
