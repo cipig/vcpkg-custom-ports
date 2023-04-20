286cac90931b9b91fd692cdb915b4c9905dea3bc
cipig <cipianon@gmail.com>
2023-04-20 23:46:28 +0000

    entt 3.9.0

--- ports/entt/portfile.cmake
+++ ports/entt/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO skypjack/entt
-    REF v3.10.3
+    REF v3.9.0
     SHA512 820215cc33a27b942a6b84ac60a6db81c24d4a3a2f6dcf3d4e55a9e4e1a80736592acb39e5e34432d9c454558a41f1ae5d5066d20102f617e8b3e98c259d6248
     HEAD_REF master
 )
