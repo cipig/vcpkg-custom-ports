55367c129975d94a6ed4620573cf4a7bedc843f5
cipig <cipianon@gmail.com>
2025-04-17 12:39:04 +0000

    update doctest to 2.4.11

--- ports/doctest/portfile.cmake
+++ ports/doctest/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO KomodoPlatform/doctest
-    REF d5aa2bfb8f00b6260296a754af3a3a98d93f7b67 #version 2.3.8
-    SHA512 2a3c66a057f810d285a6c0b09c3b2674cd3e05ffd6703fc66e799309db0ce1c055c999c90266b153ffdf3805b0e227194e2ec015a5f26117560053c1088b1b08
+    REPO doctest/doctest
+    REF ae7a13539fb71f270b87eb2e874fbac80bc8dda2 #version 2.4.11
+    SHA512 4158ee38db394bb8a8359b71bbb7c3d3dd2721c765682b92b75bf1e3865283d24a47cb1b14b9faecbbf83712075e9ecfba13df869c1a6922e5ea7c5a4a6f4e3d
     HEAD_REF master
 )
 
