bf1063ef2be25029f6c614b76f0b43ef8a936faa
cipig <cipianon@gmail.com>
2025-06-16 01:10:55 +0000

    spdlog

--- ports/spdlog/portfile.cmake
+++ ports/spdlog/portfile.cmake
@@ -1,30 +1,35 @@
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO gabime/spdlog
-    REF "v${VERSION}"
-    SHA512 21c35f4091850ea3a0cd6a24867e06e943df70d76cd5a7ec0b15a33e0e9e0cc3584ed7930e1ac6f347e7e06f0e002d0e759884eaf05310014e24ea0e0419fcc4
-    HEAD_REF v1.x
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO gabime/spdlog
+        REF v1.8.5
+        SHA512 77cc9df0c40bbdbfe1f3e5818dccf121918bfceac28f2608f39e5bf944968b7e8e24a6fc29f01bc58a9bae41b8892d49cfb59c196935ec9868884320b50f130c
+        HEAD_REF v1.x
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-        benchmark SPDLOG_BUILD_BENCH
-        wchar     SPDLOG_WCHAR_SUPPORT
-)
+        FEATURES
+        benchmark       SPDLOG_BUILD_BENCH
+        wchar           SPDLOG_WCHAR_SUPPORT
+        )
 
-# SPDLOG_WCHAR_FILENAMES can only be configured in triplet file since it is an alternative (not additive)
-if(NOT DEFINED SPDLOG_WCHAR_FILENAMES)
-    set(SPDLOG_WCHAR_FILENAMES OFF)
-endif()
-if(NOT VCPKG_TARGET_IS_WINDOWS AND SPDLOG_WCHAR_FILENAMES)
-    message(FATAL_ERROR "Build option 'SPDLOG_WCHAR_FILENAMES' is for Windows.")
+if(VCPKG_TARGET_IS_WINDOWS)
+    # configured in triplet file
+    if(NOT DEFINED SPDLOG_WCHAR_FILENAMES)
+        set(SPDLOG_WCHAR_FILENAMES ON)
+    endif()
+else()
+    if("wchar" IN_LIST FEATURES)
+        message(FATAL_ERROR "Feature 'wchar' is for Windows.")
+    elseif(SPDLOG_WCHAR_FILENAMES)
+        message(FATAL_ERROR "Build option 'SPDLOG_WCHAR_FILENAMES' is for Windows.")
+    endif()
 endif()
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" SPDLOG_BUILD_SHARED)
 
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
+        SOURCE_PATH ${SOURCE_PATH}
+        OPTIONS
         ${FEATURE_OPTIONS}
         -DSPDLOG_FMT_EXTERNAL=ON
         -DSPDLOG_INSTALL=ON
@@ -38,33 +43,26 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/spdlog)
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
-if(NOT VCPKG_BUILD_TYPE)
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/spdlog.pc" " -lspdlog" " -lspdlogd")
-endif()
+# use vcpkg-provided fmt library (see also option SPDLOG_FMT_EXTERNAL above)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/spdlog/fmt/bundled)
 
-# add support for integration other than cmake
-vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/spdlog/tweakme.h
-    "// #define SPDLOG_FMT_EXTERNAL"
-    "#ifndef SPDLOG_FMT_EXTERNAL\n#define SPDLOG_FMT_EXTERNAL\n#endif"
-)
-if(SPDLOG_WCHAR_SUPPORT)
-    vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/spdlog/tweakme.h
-        "// #define SPDLOG_WCHAR_TO_UTF8_SUPPORT"
-        "#ifndef SPDLOG_WCHAR_TO_UTF8_SUPPORT\n#define SPDLOG_WCHAR_TO_UTF8_SUPPORT\n#endif"
-    )
-endif()
-if(SPDLOG_WCHAR_FILENAMES)
-    vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/spdlog/tweakme.h
-        "// #define SPDLOG_WCHAR_FILENAMES"
-        "#ifndef SPDLOG_WCHAR_FILENAMES\n#define SPDLOG_WCHAR_FILENAMES\n#endif"
-    )
-endif()
+vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/spdlog/fmt/fmt.h
+        "#if !defined(SPDLOG_FMT_EXTERNAL)"
+        "#if 0 // !defined(SPDLOG_FMT_EXTERNAL)"
+        )
 
-file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/include/spdlog/fmt/bundled"
-    "${CURRENT_PACKAGES_DIR}/debug/include"
-    "${CURRENT_PACKAGES_DIR}/debug/share"
-)
+vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/spdlog/fmt/ostr.h
+        "#if !defined(SPDLOG_FMT_EXTERNAL)"
+        "#if 0 // !defined(SPDLOG_FMT_EXTERNAL)"
+        )
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/spdlog/fmt/chrono.h
+        "#if !defined(SPDLOG_FMT_EXTERNAL)"
+        "#if 0 // !defined(SPDLOG_FMT_EXTERNAL)"
+        )
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
+        ${CURRENT_PACKAGES_DIR}/debug/share)
+
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
